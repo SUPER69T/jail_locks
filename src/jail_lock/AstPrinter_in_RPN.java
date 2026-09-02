76ab7dfcd2d4bfe7af0ecf9bfcc7b9e84af8268d
SUPER69T <118633104+SUPER69T@users.noreply.github.com>
2026-09-02 06:02:26 +0300

    now moving to chapter 5.

--- src/jail_lock/AstPrinter_in_RPN.java
+++ src/jail_lock/AstPrinter_in_RPN.java
@@ -44,6 +44,17 @@ class AstPrinter_in_RPN implements Expr.Visitor<String> {
   }
 
   public static void main(String[] args) {
+    //region expression
+     Expr expression = new Expr.Binary(
+        new Expr.Unary(
+            new Token(TokenType.MINUS, "-", null, 1),
+            new Expr.Literal(123)),
+        new Token(TokenType.STAR, "*", null, 1),
+        new Expr.Grouping(
+            new Expr.Literal(45.67)));
+    //endregion
+
+    //region expression2
     Expr expression2 = new Expr.Binary(
             new Expr.Grouping(
                     new Expr.Binary(
@@ -57,8 +68,9 @@ class AstPrinter_in_RPN implements Expr.Visitor<String> {
                             new Expr.Literal(4),
                             new Token(TokenType.MINUS, "-", null, 1),
                             new Expr.Literal(3))));
+    //endregion
 
-
-    System.out.println(new AstPrinter_in_RPN().print_in_RPN(expression2));
+    System.out.println("expression:\n" + new AstPrinter_in_RPN().print_in_RPN(expression));
+    System.out.println("expression2:\n" + new AstPrinter_in_RPN().print_in_RPN(expression2));
   }
 }
