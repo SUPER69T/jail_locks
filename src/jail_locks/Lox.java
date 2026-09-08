a799508fdb6f537d420c6d77486647608643bc61
SUPER69T <118633104+SUPER69T@users.noreply.github.com>
2026-09-08 16:14:08 +0300

    finished chapter 7's code.

--- src/jail_locks/Lox.java
+++ src/jail_locks/Lox.java
@@ -9,7 +9,10 @@ import java.nio.file.Paths;
 import java.util.List;
 
 public class Lox {
-     static boolean hadError = false;
+    private static final Interpreter interpreter = new Interpreter();
+
+    static boolean hadError = false;
+    static boolean hadRuntimeError = false;
 
   public static void main(String[] args) throws IOException {
     if (args.length > 1) {
@@ -25,8 +28,10 @@ public class Lox {
    private static void runFile(String path) throws IOException {
     byte[] bytes = Files.readAllBytes(Paths.get(path));
     run(new String(bytes, Charset.defaultCharset()));
+
     // Indicate an error in the exit code.
     if (hadError) System.exit(65);
+    if (hadRuntimeError) System.exit(70);
   }
 
    private static void runPrompt() throws IOException {
@@ -52,7 +57,8 @@ public class Lox {
     // Stop if there was a syntax error.
     if (hadError) return;
 
-    System.out.println(new AstPrinter().print_in_PN(expression));
+    //System.out.println(new AstPrinter().print_in_PN(expression));
+    interpreter.interpret(expression);
   }
 
   static void error(int line, String message) {
@@ -70,5 +76,11 @@ public class Lox {
     } else {
       report(token.line, " at '" + token.lexeme + "'", message + ".");
     }
+  }
+
+  static void runtimeError(RuntimeError error) {
+    System.err.println(error.getMessage() +
+        "\n[line " + error.token.line + "]");
+    hadRuntimeError = true;
   }
 }
---
+++ src/jail_locks/RuntimeError.java
@@ -0,0 +1,10 @@
+package jail_locks;
+
+class RuntimeError extends RuntimeException {
+  final Token token;
+
+  RuntimeError(Token token, String message) {
+    super(message);
+    this.token = token;
+  }
+}
